67b167b439a2aa89dfe293aff90c4ecca3ce687e
Kyputraa <rizky504.rp@gmail.com>
2022-07-03 17:02:46 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+<title> PALETTE COFFE</title>
+<meta http-equiv="refresh" content="3;url=login.php">
+<head>
+
+<body>
+    
+  <!-- Jumbotron -->
+  <div class="jumbotron jumbotron-fluid text-center">
+        <div class="container">
+          <h1 class="display-4"><span class="font-weight-bold">PALETTE COFFE</span></h1>
+          <hr>
+          <p class="lead font-weight-bold">WELCOME<br> 
+          </p>
+        </div>
+      </div>
+  <!-- Akhir Jumbotron -->
+  <nav class="navbar navbar-expand-lg  bg-dark">
+        <div class="container">
+        <a class="navbar-brand text-white " ><strong></strong> </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+            </li>
+          </ul>
+        </div>
+       </div> 
+      </nav>
+</body>
+</html>
